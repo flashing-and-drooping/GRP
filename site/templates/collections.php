50d4e9f8061b74aefa7df3028e5aaa4fe726c1a4
Pedro Borges <oi@pedroborg.es>
2020-07-09 13:58:02 -0300

    Remove margin from last figure

--- site/templates/collections.php
+++ site/templates/collections.php
@@ -2,7 +2,9 @@
 
 <div class="sm:flex sm:justify-end pt-offset">
 
-  <main class="w-full sm:w-3/4">
+  <main class="w-full sm:w-3/4 pb-3">
+
+    <h1 class="sr-only"><?= $page->title()->html() ?></h1>
       
     <?php snippet('collections') ?>
 
