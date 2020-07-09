61daeb31d60ff35069dea238012f181b51670881
Pedro Borges <oi@pedroborg.es>
2020-07-09 10:58:44 -0300

    Remove focus ring from inputs

--- site/snippets/private.php
+++ site/snippets/private.php
@@ -10,11 +10,11 @@
   <form id="login" class="hidden" aria-hidden="true">
 
     <label class="block mt-2 mb-1">
-      Username <input type="text" name="username">
+      Username <input type="text" name="username" class="focus:outline-none">
     </label>
 
     <label class="block mb-1">
-      Password <input type="password" name="password">
+      Password <input type="password" name="password" class="focus:outline-none">
     </label>
 
   </form>
