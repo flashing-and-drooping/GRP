cb0c48ac8012e72778c252f7abbc73cadb26c32a
Pedro Borges <oi@pedroborg.es>
2020-08-05 15:19:10 -0300

    Remove old homepage

--- site/snippets/home.php
+++
@@ -1,9 +0,0 @@
-<?php if ($image = $page->cover()->toFile()) : ?>
-<figure class="w-full h-fill mb-3">
-  <img
-    class="w-full h-full object-cover"
-    src="<?= $image->resize(700, 700, 90)->url() ?>"
-    srcset="<?= $image->resize(700, 700, 90)->url() ?> 1x, <?= $image->resize(1400, 1400, 90)->url() ?> 2x"
-    alt="">
-</figure>
-<?php endif ?>
